9b62b0f497d9cdaca89e71ac72f1caa24ef6196d
Dylan Kay <dylan@tech.melbourne>
2022-07-02 18:59:18 +0200

    Use custom collector, use managed token manager contract

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
     BASE_CLUSTER: process.env.BASE_CLUSTER,
     STAKE_POOL_ADDRESS: process.env.STAKE_POOL_ADDRESS,
     REWARD_DISTRIBUTOR_ADDRESS: process.env.REWARD_DISTRIBUTOR_ADDRESS,
+    TOKEN_MANAGER_ADDRESS: process.env.TOKEN_MANAGER_ADDRESS,
     REWARD_MANAGER: process.env.REWARD_MANAGER,
+    COLLECTOR: process.env.COLLECTOR,
   },
   async rewrites() {
     return [
@@ -20,7 +22,7 @@ const nextConfig = {
           },
           {
             type: 'host',
-            value: 'stake.unfrgtn.space'
+            value: 'stake.unfrgtn.space',
           },
         ],
       },
